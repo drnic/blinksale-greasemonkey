85a2b02266835eb03ea3abec591ea9f08033614b
Dr Nic Williams <drnicwilliams@gmail.com>
2008-10-11 13:29:08 +1000

    header txt for new column

--- src/invoices.js
+++ src/invoices.js
@@ -4,6 +4,9 @@ Blinksale.Invoices = function() {
     var element = $(element);
     var tag = element.childElements()[0].tagName;
     var field = new Element(tag, { "name": "converted_amount" });
+    if (tag == "TH") {
+      field.update("Converted Amount");
+    }
     element.insert(field);
   });
 };
