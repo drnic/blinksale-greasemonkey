8300f7b6e872b53325d1dd87e76822f89dbe60e7
Dr Nic Williams <drnicwilliams@gmail.com>
2008-10-11 15:36:32 +1000

    started conversion_rate code

---
+++ src/conversion_rate.js
@@ -0,0 +1,12 @@
+var Blinksale = Blinksale || {};
+Blinksale.ConversionRate = {};
+
+// Converts an amount (100, '100' or '$100') from a currency ('USD')
+// to another currency ('AUD') and stores the results within a DOM element.
+// If the conversion rate is already available then the element
+// is updated immediately. Otherwise, this method returns immediately, but
+// the element update will occur when the conversion rate data is available.
+// That is, this method is non-blocking.
+Blinksale.ConversionRate.convertElement = function(element, amount, from, to) {
+  
+};
